ee9ad6fecb47dd00fba52b2c711bcd394047ef76
Christopher Lozzi <clozzi12@gmail.com>
2023-11-09 11:07:53 -0500

    initial GET

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,12 +2,15 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokeData }) {
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {pokeData.map((poke) => (
+        <PokemonCard key={poke.id} name={poke.name} hp={poke.hp} sprites={poke.sprites} />
+      ))}
     </Card.Group>
   );
 }
-
+ 
 export default PokemonCollection;
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokeData, setPokeData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+    .then(r => r.json())
+    .then(data => setPokeData(data))
+  })
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,7 +21,7 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokeData={pokeData} />
     </Container>
   );
 }
